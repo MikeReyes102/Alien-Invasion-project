b73b464eb322fc14cc9c7014288253d8ff32bea8
MikeReyes102 <97359256+MikeReyes102@users.noreply.github.com>
2022-10-12 15:57:05 -0400

    AI v 1.8 fleet movement

--- Alien Invasion/alien_invasion.py
+++ Alien Invasion/alien_invasion.py
@@ -31,6 +31,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
  
             
@@ -79,7 +80,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
         print(len(self.bullets))
-        
+    
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
+       
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # Create an alien and find the number of aliens in a row.
