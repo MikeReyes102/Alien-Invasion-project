50bcca8b9ce165e6fe41393351fbad82d0156af2
MikeReyes102 <97359256+MikeReyes102@users.noreply.github.com>
2022-10-13 06:15:23 -0400

    AI v 1.17 scoring and leveling

--- Alien Invasion/alien_invasion.py
+++ Alien Invasion/alien_invasion.py
@@ -69,6 +69,8 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
+            self.sb.prep_level()
             
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -191,15 +193,23 @@ class AlienInvasion:
     def _check_bullet_alien_collisions(self):
         """Respond to bullet-alien collisions."""
         # Remove any bullets and aliens that have collided.
-        # Check for any bullets that have hit aliens.
-        #   If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points *len(aliens)
+            self.sb.prep_score()
+            self.sb.prep_high_score()
         
         if not self.aliens:
         # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+            
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
     
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
